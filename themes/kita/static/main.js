83bd490283d408b3d2047975847f47df6c8468d7
MTLKS <43807318+MTLKS@users.noreply.github.com>
2025-08-10 20:01:40 +0800

    add flip on load

--- themes/kita/static/main.js
+++ themes/kita/static/main.js
@@ -1,7 +1,7 @@
 const coin = document.getElementById("coin");
 let currentAngle = 0;
 
-coin.addEventListener("click", () => {
+function flipCoin() {
     let isHeads = Math.random() > 0.5;
     let flips = 3; // number of full spins before result
     let angle = flips * 360 + (isHeads ? 0 : 180);
@@ -16,4 +16,7 @@ coin.addEventListener("click", () => {
     coin.style.transform = `rotateY(${angle}deg)`;
 
     currentAngle = isHeads ? 0 : 180;
-});
+}
+
+coin.addEventListener("click", flipCoin);
+window.addEventListener("load", flipCoin);

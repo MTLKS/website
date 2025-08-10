a3cca72818c7191faafa2e9e7406d37709dde9c5
MTLKS <43807318+MTLKS@users.noreply.github.com>
2025-08-10 19:57:49 +0800

    add coinflip

---
+++ themes/kita/static/main.js
@@ -0,0 +1,19 @@
+const coin = document.getElementById("coin");
+let currentAngle = 0;
+
+coin.addEventListener("click", () => {
+    let isHeads = Math.random() > 0.5;
+    let flips = 3; // number of full spins before result
+    let angle = flips * 360 + (isHeads ? 0 : 180);
+
+    coin.style.transition = "none";
+    coin.style.transform = `rotateY(${currentAngle}deg)`;
+
+    void coin.offsetWidth;
+
+    coin.style.transition = "transform 1s ease-in-out";
+
+    coin.style.transform = `rotateY(${angle}deg)`;
+
+    currentAngle = isHeads ? 0 : 180;
+});
